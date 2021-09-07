9810708fb5324455c418e0b576327bd170f82f91
B-aslan0 <bhb0407@naver.com>
2021-09-07 21:07:19 +0900

    코딩테스트

--- src/com/company/ioc/study/resection1/renumber4/Main.java
+++ src/com/company/ioc/study/resection1/renumber4/Main.java
@@ -1,29 +1,54 @@
 package com.company.ioc.study.resection1.renumber4;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 //  n개의 단어를 주어지면 단어를 거꾸로 출력하기
 public class Main {
-    public String solution(String[] str) {
-        String answer = "";
-        for (String x : str) {
+    public ArrayList<String> solution(int n ,String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
 
+        for (String x : str) {
+            char[] c = x.toCharArray();
+            int sp = 0;
+            int ep = x.length() -1;
+            while(sp<ep){
+                char temp = c[sp];
+                c[sp] = c[ep];
+                c[ep] = temp;
+                sp++;
+                ep--;
+            }
+            String temp = String.valueOf(c);
+            answer.add(temp);
         }
-        int sp = 0;
-        int ep =
+        return answer;
+    }
+
+    public ArrayList<String> solutions(int n, String[] str ) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            String temp = new StringBuilder().reverse().toString();
+            answer.add(temp);
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        String[] str = new String[];
+        String[] str = new String[num];
         for (int i = 0; i < num; i++) {
             str[i] = scanner.next();
         }
-        for (String x: main.solution()
+        for (String x: main.solution(num, str)
              ) {
-
+            System.out.println(x);
+        }
+        for (String x: main.solutions(num, str)
+             ) {
+            System.out.println(x);
         }
     }
 }
